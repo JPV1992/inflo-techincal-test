229990191e8c2bc64913545b81c8c5ed0ec2a67a
Jason <45313246+JPV1992@users.noreply.github.com>
2024-02-20 13:24:31 +0000

    Added Date of Birth field to Users List

--- UserManagement.Data/DataContext.cs
+++ UserManagement.Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using UserManagement.Models;
 
@@ -14,17 +15,17 @@ public class DataContext : DbContext, IDataContext
     protected override void OnModelCreating(ModelBuilder model)
         => model.Entity<User>().HasData(new[]
         {
-            new User { Id = 1, Forename = "Peter", Surname = "Loew", Email = "ploew@example.com", IsActive = true },
-            new User { Id = 2, Forename = "Benjamin Franklin", Surname = "Gates", Email = "bfgates@example.com", IsActive = true },
-            new User { Id = 3, Forename = "Castor", Surname = "Troy", Email = "ctroy@example.com", IsActive = false },
-            new User { Id = 4, Forename = "Memphis", Surname = "Raines", Email = "mraines@example.com", IsActive = true },
-            new User { Id = 5, Forename = "Stanley", Surname = "Goodspeed", Email = "sgodspeed@example.com", IsActive = true },
-            new User { Id = 6, Forename = "H.I.", Surname = "McDunnough", Email = "himcdunnough@example.com", IsActive = true },
-            new User { Id = 7, Forename = "Cameron", Surname = "Poe", Email = "cpoe@example.com", IsActive = false },
-            new User { Id = 8, Forename = "Edward", Surname = "Malus", Email = "emalus@example.com", IsActive = false },
-            new User { Id = 9, Forename = "Damon", Surname = "Macready", Email = "dmacready@example.com", IsActive = false },
-            new User { Id = 10, Forename = "Johnny", Surname = "Blaze", Email = "jblaze@example.com", IsActive = true },
-            new User { Id = 11, Forename = "Robin", Surname = "Feld", Email = "rfeld@example.com", IsActive = true },
+            new User { Id = 1, Forename = "Peter", Surname = "Loew", DateOfBirth = DateTime.Parse("1992-07-16"), Email = "ploew@example.com", IsActive = true },
+            new User { Id = 2, Forename = "Benjamin Franklin", Surname = "Gates",  DateOfBirth = DateTime.Parse("1989-01-23"),Email = "bfgates@example.com", IsActive = true },
+            new User { Id = 3, Forename = "Castor", Surname = "Troy",  DateOfBirth = DateTime.Parse("2001-12-03"),Email = "ctroy@example.com", IsActive = false },
+            new User { Id = 4, Forename = "Memphis", Surname = "Raines", DateOfBirth = DateTime.Parse("1997-07-07"),Email = "mraines@example.com", IsActive = true },
+            new User { Id = 5, Forename = "Stanley", Surname = "Goodspeed",  DateOfBirth = DateTime.Parse("1964-12-13"), Email = "sgodspeed@example.com", IsActive = true },
+            new User { Id = 6, Forename = "H.I.", Surname = "McDunnough",  DateOfBirth = DateTime.Parse("1990-09-11"),Email = "himcdunnough@example.com", IsActive = true },
+            new User { Id = 7, Forename = "Cameron", Surname = "Poe",  DateOfBirth = DateTime.Parse("1984-10-30"),Email = "cpoe@example.com", IsActive = false },
+            new User { Id = 8, Forename = "Edward", Surname = "Malus",  DateOfBirth = DateTime.Parse("1995-05-02"),Email = "emalus@example.com", IsActive = false },
+            new User { Id = 9, Forename = "Damon", Surname = "Macready",  DateOfBirth = DateTime.Parse("1953-02-28"), Email = "dmacready@example.com", IsActive = false },
+            new User { Id = 10, Forename = "Johnny", Surname = "Blaze",  DateOfBirth = DateTime.Parse("2004-02-20"), Email = "jblaze@example.com", IsActive = true },
+            new User { Id = 11, Forename = "Robin", Surname = "Feld",  DateOfBirth = DateTime.Parse("1999-12-30"), Email = "rfeld@example.com", IsActive = true },
         });
 
     public DbSet<User>? Users { get; set; }
