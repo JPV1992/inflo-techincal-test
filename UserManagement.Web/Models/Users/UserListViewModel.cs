31952301f3ebb67e314397600b023cd3cc56fc4f
Jason <45313246+JPV1992@users.noreply.github.com>
2024-02-28 21:04:33 +0000

    Added Client Side validation

--- UserManagement.Web/Models/Users/UserListViewModel.cs
+++ UserManagement.Web/Models/Users/UserListViewModel.cs
@@ -10,13 +10,24 @@ public class UserListViewModel
 public class UserListItemViewModel
 {
     public long Id { get; set; }
+
+    [Required(ErrorMessage = "Please add a Forename")]
+    [RegularExpression(@"^[a-zA-Z\-]+$", ErrorMessage = "Invalid characters in Forename.")]
     public string? Forename { get; set; }
+
+    [Required(ErrorMessage = "Please add a Surname")]
+    [RegularExpression(@"^[a-zA-Z\-]+$", ErrorMessage = "Invalid characters in Surname.")]
     public string? Surname { get; set; }
+
+    [Required(ErrorMessage = "Please add an email address")]
     [EmailAddress(ErrorMessage = "Invalid Email Address")]
     public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Please add a date of birth in the format yyyy-MM-dd")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public string? DateOfBirth { get; set; }
+
     [Required(ErrorMessage = "Please select the account status")]
     public bool IsActive { get; set; }
 }
